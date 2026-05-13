e2b3224cab8761a7728054650c9f13f6b88e0e64
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-05-13 15:33:51 +0530

    feat:calculates 20 days wages

--- Employee.java
+++ Employee.java
@@ -7,23 +7,28 @@ public class Employee{
         System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
         int monthlySalary=0;
         Random random=new Random();
- for(int i=0;i<20;i++){     
+        int worked_hours=0;
+        int worked_days=0;
+ while(worked_days<20 && worked_hours<100){    
  int attendence=random.nextInt(2);
 if(attendence==1){
             int workingType=random.nextInt(2)+1;
 switch (workingType) {
     case 1:
-        monthlySalary=monthlySalary+20*8;
+        monthlySalary=monthlySalary+20*4;
+        worked_hours=worked_hours+4;
         break;
 
     default:
-        System.out.println("Today Employee is Full Time");
+        monthlySalary=monthlySalary+20*8;
+        worked_hours=worked_hours+8;
         break;
 }
 }
 else{
     monthlySalary=monthlySalary+0;
 }
+worked_days++;
  }
  System.out.println("In 20 Days he earned "+monthlySalary);
     }
