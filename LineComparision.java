af92a2d80982284c433fa0bbabe29d72b6dac52e
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-05-13 16:17:25 +0530

    feat:that calculates line height

--- LineComparision.java
+++ LineComparision.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class LineComparision {
     public static void main(String[] args) {
-      System.out.println("welcome to line comparision program on main");  
+      System.out.println("welcome to line comparision program on main"); 
+         Scanner sc=new Scanner(System.in);
+        int x1=sc.nextInt();
+        int y1=sc.nextInt();
+        int x2=sc.nextInt();
+        int y2=sc.nextInt();
+        int x3=sc.nextInt();
+        int y3=sc.nextInt();
+        int x4=sc.nextInt();
+        int y4=sc.nextInt();
+        double lineOneHeight=Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
+        double lineTwoHeight=Math.sqrt(Math.pow((x4-x3),2)+Math.pow((y4-y3),2));
+        System.out.println("The Height of Line one is : "+lineOneHeight);
+        System.out.println("the height of second line is : "+lineTwoHeight);
     }
 }
