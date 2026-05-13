58ea762e06b8e94619d254b243b5bfba5464fdc3
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-05-13 16:05:52 +0530

    feat:using classes

--- Employee.java
+++ Employee.java
@@ -1,28 +1,56 @@
 import java.util.Random;
 
 public class Employee{
-    public static int isPartTime=1;
-    public static int isFullTime=2;
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-        int monthlySalary=0;
-        Random random=new Random();
-        int worked_hours=0;
-        int worked_days=0;
- while(worked_days<20 && worked_hours<100){    
+        System.out.println("Welcome to Employee Wage Computation Program on Master Branch"); 
+        String company=args[0];
+        int workingHours=Integer.parseInt(args[1]);
+        int workingDays=Integer.parseInt(args[2]);
+        int dailyWages=Integer.parseInt(args[3]);
+        int partTimeHours=Integer.parseInt(args[4]);
+        int fullTimeHours=Integer.parseInt(args[5]);
+        EmployeeWagesCalculation empWages=new EmployeeWagesCalculation(company,workingHours,workingDays,dailyWages,partTimeHours,fullTimeHours);
+        System.out.println(empWages.getMonthlySalarr());
+    }
+}
+class EmployeeWagesCalculation{
+    public static Random random=new Random();
+    public static int isFullTime=1;
+    public static int isPartTime=2;
+    private String company;
+    private int workingHours;
+    private int workingDays;
+    private int dailyWages;
+    private int partTimeHours;
+    private int fullTimeHours;
+    public EmployeeWagesCalculation(String company,int workingHours,int workingDays,int dailyWages,int partTimeHours,int fullTimeHours){
+ this.company=company;
+ this.workingHours=workingHours;
+ this.workingDays=workingDays;
+ this.dailyWages=dailyWages;
+ this.partTimeHours=partTimeHours;
+ this.fullTimeHours=fullTimeHours;
+    }
+   public int getMonthlySalarr(){
+    int monthlySalary=0;
+    int worked_days=0;
+    int worked_hours=0;
+     while(worked_days<workingDays && worked_hours<workingHours){    
  int attendence=random.nextInt(2);
 if(attendence==1){
-            int workingType=random.nextInt(2)+1;
+    int workingType=random.nextInt(2)+1;
 switch (workingType) {
     case 1:
-        monthlySalary=monthlySalary+20*4;
-        worked_hours=worked_hours+4;
+        monthlySalary=monthlySalary+fullTimeHours*dailyWages;
+        worked_hours=worked_hours+fullTimeHours;
         break;
 
-    default:
-        monthlySalary=monthlySalary+20*8;
-        worked_hours=worked_hours+8;
+    case 2:
+        monthlySalary=monthlySalary+partTimeHours*dailyWages;
+        worked_hours=worked_hours+partTimeHours;
         break;
+    default :
+      break;    
 }
 }
 else{
@@ -30,6 +58,6 @@ else{
 }
 worked_days++;
  }
- System.out.println("In 20 Days he earned "+monthlySalary);
-    }
+return monthlySalary;
+   } 
 }
