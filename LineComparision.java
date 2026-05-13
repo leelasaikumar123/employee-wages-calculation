5be41c458f553980708ee9cd5b3c057d271c5437
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-05-13 17:02:25 +0530

    feat:distance program using classes

--- LineComparision.java
+++ LineComparision.java
@@ -12,8 +12,11 @@ public class LineComparision {
         int y3=sc.nextInt();
         int x4=sc.nextInt();
         int y4=sc.nextInt();
-        double lineOneHeight=Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
-        double lineTwoHeight=Math.sqrt(Math.pow((x4-x3),2)+Math.pow((y4-y3),2));
+        CalculationLine cl1=new CalculationLine(x1, y1, x2, y2);
+CalculationLine cl2=new CalculationLine(x3, y3, x4, y4);
+
+double lineOneHeight=cl1.lineHeight();
+double lineTwoHeight=cl2.lineHeight();
         System.out.println("The Height of Line one is : "+lineOneHeight);
         System.out.println("the height of second line is : "+lineTwoHeight);
         if(Double.valueOf(lineOneHeight).equals(Double.valueOf(lineTwoHeight))){
@@ -34,4 +37,21 @@ public class LineComparision {
         }
         }
     }
+        class CalculationLine{
+           private int x1;
+          private   int y1;
+           private  int x2;
+           private  int y2;
+             CalculationLine(int x1,int y1,int x2,int y2){
+                this.x1=x1;
+                this.y1=y1;
+                this.x2=x2;
+                this.y2=y2;
+            }
+            public double lineHeight(){
+                return Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
+            }
+
+        }
+    
 
