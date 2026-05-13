85bc9952ca8132091f13fe60dcdab4dc36f9ba03
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-05-13 14:55:55 +0530

    feat:tells weather an employee is present or not

--- Employee.java
+++ Employee.java
@@ -3,6 +3,14 @@ import java.util.Random;
 public class Employee{
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-        
+        Random random=new Random();
+        int attendence=random.nextInt(2);
+        System.out.println(attendence);
+        if(attendence==0){
+            System.out.println("Employee is absent");
+        }
+        else{
+            System.out.println("Employee is Present");
+        }
     }
 }
